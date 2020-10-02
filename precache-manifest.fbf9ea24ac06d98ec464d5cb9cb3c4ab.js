b681a6a8f7d8c38683062e22e3103b1294a586f3
BitFlex <68299707+BitFlexFinTech@users.noreply.github.com>
2020-10-02 16:43:46 +0500

    Final Build

---
+++ precache-manifest.fbf9ea24ac06d98ec464d5cb9cb3c4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c721053f1043a1c6fa4ecec4bb72f0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5d5576ff36039d3cc1e",
+    "url": "/static/css/main.8c1e1f75.chunk.css"
+  },
+  {
+    "revision": "c330db552eb62497bee2",
+    "url": "/static/js/2.5ec843df.chunk.js"
+  },
+  {
+    "revision": "fddc5c707eccc23815a8617cf795b106",
+    "url": "/static/js/2.5ec843df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d5576ff36039d3cc1e",
+    "url": "/static/js/main.16d7c39b.chunk.js"
+  },
+  {
+    "revision": "a8202d2c482926c65fe9",
+    "url": "/static/js/runtime-main.f3d78015.js"
+  }
+]);
